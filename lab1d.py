3bc123ed50b916e7ee2b3f69dd247f631d3e532c
sshrestha183 <sshrestha183@myseneca.ca>
2026-05-08 11:16:57 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: Lab1d math operations'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
